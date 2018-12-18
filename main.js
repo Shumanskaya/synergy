5d680ba210b1dfab3507b95e96de4ddc8930fab8
Evgenia <myemail@mail.ru>
2018-12-18 17:14:03 +0300

    add file

---
+++ main.js
@@ -0,0 +1,22 @@
+    $(document).ready(function() {
+        
+        $("a#inline").fancybox({
+            'hideOnContentClick': true
+        });
+        $("a#clickMe").fancybox({
+            'hideOnContentClick': true
+        });
+        $("a#clickVideo").fancybox({
+            'type': "iframe",
+            'hideOnContentClick': true
+        });
+        $("a#clickMaps").fancybox({
+            'type': "iframe",
+            'hideOnContentClick': true
+        });
+        
+    });
+
+
+   
+
